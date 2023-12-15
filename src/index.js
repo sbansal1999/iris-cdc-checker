edfeed1fee137038d5822c46dba2a4a6eab41eea
Satyam Bansal <sbansal1999@gmail.com>
2023-12-15 18:50:39 +0530

    Add functionality for Internship companies.

--- src/index.js
+++ src/index.js
@@ -15,54 +15,64 @@ const supabase = createClient(
 );
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-async function getCurrentListedCompanies() {
+const placementCompaniesTable = "placement_companies";
+const internshipCompaniesTable = "internship_companies";
+
+async function getCurrentListedCompaniesByCookie(cookie) {
   try {
-    const websiteData = await axios.get(
-      "https://iris.nitk.ac.in/hrms/placement/dashboard",
-      {
-        headers: {
-          accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "accept-language":
-            "en-IN,en;q=0.9,hi-IN;q=0.8,hi;q=0.7,en-GB;q=0.6,en-US;q=0.5,ar;q=0.4,ru;q=0.3",
-          "cache-control": "no-cache",
-          pragma: "no-cache",
-          "sec-ch-ua":
-            '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Linux"',
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "same-origin",
-          "sec-fetch-user": "?1",
-          "upgrade-insecure-requests": "1",
-          cookie: process.env.IRIS_COOKIE,
-          Referer: "https://iris.nitk.ac.in/hrms/",
-          "Referrer-Policy": "strict-origin-when-cross-origin",
-        },
-        body: null,
-        method: "GET",
-      }
-    );
+    async function getCompanyDataByCookie(cookie) {
+      const websiteData = await axios.get(
+        "https://iris.nitk.ac.in/hrms/placement/dashboard",
+        {
+          headers: {
+            accept:
+              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language":
+              "en-IN,en;q=0.9,hi-IN;q=0.8,hi;q=0.7,en-GB;q=0.6,en-US;q=0.5,ar;q=0.4,ru;q=0.3",
+            "cache-control": "no-cache",
+            pragma: "no-cache",
+            "sec-ch-ua":
+              '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            cookie: cookie,
+            Referer: "https://iris.nitk.ac.in/hrms/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+          },
+          body: null,
+          method: "GET",
+        }
+      );
 
-    const html = websiteData.data;
-    const $ = cheerio.load(html);
-    const data = $("#upcoming tbody tr");
-    const currentCompanies = [];
-    data.each((_, element) => {
-      const company = $(element).find("td:nth-child(1) strong").text();
-      currentCompanies.push(company);
-    });
+      const html = websiteData.data;
+      const $ = cheerio.load(html);
+      const data = $("#upcoming tbody tr");
+      const currentCompanies = [];
+      data.each((_, element) => {
+        const company = $(element).find("td:nth-child(1) strong").text();
+        currentCompanies.push(company);
+      });
 
-    return currentCompanies;
+      return currentCompanies;
+    }
+
+    return await getCompanyDataByCookie(cookie);
   } catch (error) {
     return error;
   }
 }
 
-async function getCompaniesFromDB() {
+async function getCompaniesFromDB(internship = false) {
   try {
-    const data = await (await supabase.from("companies").select()).data;
+    const tableName = internship
+      ? internshipCompaniesTable
+      : placementCompaniesTable;
+    const data = await (await supabase.from(tableName).select()).data;
     const companies = data.map((obj) => obj.name);
     return companies;
   } catch (error) {
@@ -71,13 +81,16 @@ async function getCompaniesFromDB() {
   }
 }
 
-async function setCompaniesToDB(companies) {
+async function setCompaniesToDB(companies, internship = false) {
+  const tableName = internship
+    ? internshipCompaniesTable
+    : placementCompaniesTable;
   //delete existing data
-  await supabase.from("companies").delete().select().like("name", "*");
+  await supabase.from(tableName).delete().select().like("name", "*");
 
   //add new companies
   await supabase
-    .from("companies")
+    .from(tableName)
     .insert(companies.map((company) => ({ name: company })));
 }
 
@@ -86,8 +99,7 @@ async function getEmailsFromDB() {
     const data = await (await supabase.from("emails").select()).data;
     const emails = data.map((obj) => obj.email);
     return emails;
-  }
-  catch (error) {
+  } catch (error) {
     console.log("some error" + error);
     return error;
   }
@@ -95,19 +107,29 @@ async function getEmailsFromDB() {
 
 app.get("/scrape", async (req, res) => {
   try {
-    const currentListedCompanies = await getCurrentListedCompanies();
-    const storedCompanies = await getCompaniesFromDB();
-
-    const newCompanies = currentListedCompanies.filter(
-      (company) => !storedCompanies.includes(company)
+    const currentListedPlacementCompanies =
+      await getCurrentListedCompaniesByCookie(
+        process.env.IRIS_PLACEMENT_COOKIE
+      );
+    const storedPlacementCompanies = await getCompaniesFromDB();
+    const newPlacementCompanies = currentListedPlacementCompanies.filter(
+      (company) => !storedPlacementCompanies.includes(company)
     );
 
-    console.log(currentListedCompanies);
-    console.log(storedCompanies);
-    console.log(newCompanies);
+    const currentListedInternshipCompanies =
+      await getCurrentListedCompaniesByCookie(
+        process.env.IRIS_INTERNSHIP_COOKIE
+      );
+    const storedInternshipCompanies = await getCompaniesFromDB(true);
+    const newInternshipCompanies = currentListedInternshipCompanies.filter(
+      (company) => !storedInternshipCompanies.includes(company)
+    );
 
-    if (newCompanies.length > 0) {
-      await setCompaniesToDB(currentListedCompanies);
+    console.log(newInternshipCompanies);
+
+    if (newPlacementCompanies.length > 0 || newInternshipCompanies.length > 0) {
+      await setCompaniesToDB(currentListedPlacementCompanies);
+      await setCompaniesToDB(currentListedInternshipCompanies, true);
       const emails = await getEmailsFromDB();
       console.log(emails);
       for (const email of emails) {
@@ -115,11 +137,11 @@ app.get("/scrape", async (req, res) => {
           from: "Iris-CDC-Checker <iris-cdc-checker@mindfuelclub.tech>",
           to: [email],
           subject: "Yoo IRIS has new companies!",
-          text: `Check out the new companies at IRIS: ${newCompanies}`,
+          text: `Check out the new placement companies at IRIS: ${newPlacementCompanies} \n\n Check out the new internship companies at IRIS: ${newInternshipCompanies}`,
         });
-      };
+      }
     }
-    res.json(newCompanies);
+    res.json(newPlacementCompanies);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
